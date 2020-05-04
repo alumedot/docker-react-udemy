65d04c5b85ae09e1c2696340b5a138e4461b8b6a
Alexey Kozlov <alexey.kozlov@orange.com>
2020-05-04 15:15:13 +0300

    change app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to lalka .
+          Edit <code>src/App.js</code> and save to lalala, that's _good_.
         </p>
         <a
           className="App-link"
